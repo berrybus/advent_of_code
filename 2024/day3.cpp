89380e597fd9391c31e49313441ee054f7f80a4c
Nancy Zhang <nancy.zhang143@gmail.com>
2024-12-28 18:00:10 -0500

    day 3 part 2

--- 2024/day3.cpp
+++ 2024/day3.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <regex>
+#include <string>
 #include <sys/_types/_u_int64_t.h>
 #include <utility>
+
+bool isNumber(const std::string &str) {
+  return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+}
 
 int main() {
   std::string line;
@@ -13,18 +18,37 @@ int main() {
     input += line;
   }
 
-  std::regex mult(R"(mul\(\d+,\d+\))");
-  std::smatch match;
   std::vector<std::pair<int, int>> matches;
 
-  std::string::const_iterator search_start(input.begin());
-  while (std::regex_search(search_start, input.cend(), match, mult)) {
-    std::string inner = match.str().substr(4, match.str().size() - 5);
-    size_t comma_pos = inner.find(",");
-    int num1 = std::stoi(inner.substr(0, comma_pos));
-    int num2 = std::stoi(inner.substr(comma_pos + 1));
-    matches.emplace_back(num1, num2);
-    search_start = match.suffix().first;
+  size_t start = 0;
+
+  bool shouldAdd = true;
+  while (start < input.size() - 5) {
+    if (shouldAdd && input.substr(start, 4) == "mul(") {
+      size_t close = input.find_first_of(")", start + 4);
+      std::string inner = input.substr(start + 4, close - start - 4);
+      size_t comma_pos = inner.find(",");
+      std::string num1_str = inner.substr(0, comma_pos);
+      std::string num2_str = inner.substr(comma_pos + 1);
+      if (comma_pos < inner.size() && isNumber(num1_str) &&
+          isNumber(num2_str)) {
+        int num1 = std::stoi(num1_str);
+        int num2 = std::stoi(num2_str);
+        matches.emplace_back(num1, num2);
+        start = close + 1;
+      } else {
+        start++;
+      }
+      // comment the below out if doing part t 1
+    } else if (input.substr(start, 4) == "do()") {
+      shouldAdd = true;
+      start += 4;
+    } else if (input.substr(start, 7) == "don't()") {
+      shouldAdd = false;
+      start += 7;
+    } else {
+      start++;
+    }
   }
 
   u_int64_t tot = 0;
